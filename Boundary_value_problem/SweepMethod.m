367eab14db68dcde7f022712a4de0f6d0b7d0b15
Dariya Tkacheva <dariya.tkachva@gmail.com>
2018-06-26 22:04:57 +0300

    modified 'Boundary_value_problem'

--- Boundary_value_problem/SweepMethod.m
+++ Boundary_value_problem/SweepMethod.m
@@ -1,18 +1,18 @@
-function [ m, k ] = SweepMethod( N, h, p1, d1 )
+function [m, k] = SweepMethod(N, h, p1, d1)
 
-m=zeros(1,N);
-k=zeros(1,N);
+m = zeros(1, N);
+k = zeros(1, N);
 
-m(1)=p1;
-k(1)=d1;
+m(1) = p1;
+k(1) = d1;
 
-for i=2:1:N
-    ai=1-h*p(i)/2;
-    bi=1+(p(i)*h)/2;
-    ci=2-h^2*q(i);
+for i = 2 : 1 : N
+    ai = 1 - h * p(i) / 2;
+    bi = 1 + (p(i) * h) / 2;
+    ci = 2 - h^2 * q(i);
 
-    m(i)= bi/(ci-ai*m(i-1));
-    k(i)=((ai*k(i-1))-(h^2*f(i)))/(ci-(ai*m(i-1)));
+    m(i )= bi / (ci - ai * m(i - 1));
+    k(i) = ((ai * k(i - 1)) - (h^2 * f(i))) / (ci - (ai * m(i - 1)));
 end;
 
 end
